29f679577b5da9cbdcc8428750ec15e32de9d960
Aleksandr Sukhomlinov <iduwawl@gmail.com>
2024-01-30 15:38:55 +0300

    chore: Add other game's rules

--- brain_games/rules.py
+++ brain_games/rules.py
@@ -1,2 +1,7 @@
-THREE_CORRECT_ANSWER = 3
+COUNT_CORRECT_ANSWER = 3
 EVEN_GAME_RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
+CALC_GAME_RULE = "What is the result of the expression?"
+GCD_GAME_RULE = "Find the greatest common divisor of given numbers."
+PROGRESSION_GAME_RULE = "What number is missing in the progression?"
+PRIME_GAME_RULE = ('Answer "yes" if given number is prime. '
+                   'Otherwise answer "no".')
