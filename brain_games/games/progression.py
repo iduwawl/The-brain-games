6cd1dbb84aed4a5ede103eb4bd7edbf126155358
Aleksandr Sukhomlinov <iduwawl@gmail.com>
2024-02-13 16:08:56 +0300

    refactor: add sliced_progression var

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -11,7 +11,7 @@ MIN_INDEX_MISS_NUM = 0
 
 
 def get_progression(first_num, difference, how_many_nums) -> list:
-    return [x for x in range(first_num, how_many_nums, difference)][0:10]
+    return [x for x in range(first_num, how_many_nums, difference)]
 
 
 def get_progression_with_hiden_num(progression, index_num_to_hide) -> list:
@@ -28,9 +28,10 @@ def get_question_and_correct_answer():
     difference = randint(MIN_DIFF_PROGRESSION, MAX_DIFF_PROGRESSION)
     last_num = randint(MIN_LAST_NUM, MAX_LAST_NUM)
     progression = get_progression(first_num, difference, last_num)
+    sliced_progression = progression[0:10]
     index_of_miss_num = randint(MIN_INDEX_MISS_NUM, len(progression) - 1)
-    correct_answer = progression[index_of_miss_num]
+    correct_answer = sliced_progression[index_of_miss_num]
     progression_with_hiden_num = get_progression_with_hiden_num(
-        progression, index_of_miss_num)
+        sliced_progression, index_of_miss_num)
     question = get_progression_converted_to_str(progression_with_hiden_num)
     return question, str(correct_answer)
