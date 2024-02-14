a4756970dbaa4d19c50b5e7564e42966b484c9e8
Aleksandr Sukhomlinov <iduwawl@gmail.com>
2024-02-14 15:47:11 +0300

    refactor: Delete sliced_progression and add AMOUNT_OF_NUM var

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,8 +3,7 @@ from random import randint
 GAME_RULE = "What number is missing in the progression?"
 MIN_FIRST_NUM = 1
 MAX_FIRST_NUM = 10
-MIN_AMOUNT_OF_NUM = 25
-MAX_AMOUNT_OF_NUM = 30
+AMOUNT_OF_NUM = 10
 MIN_DIFF_PROGRESSION = 2
 MAX_DIFF_PROGRESSION = 9
 MIN_INDEX_MISS_NUM = 0
@@ -30,12 +29,10 @@ def get_progression_converted_to_str(progression) -> str:
 def get_question_and_correct_answer():
     first_num = randint(MIN_FIRST_NUM, MAX_FIRST_NUM)
     difference = randint(MIN_DIFF_PROGRESSION, MAX_DIFF_PROGRESSION)
-    amount_of_nums = randint(MIN_AMOUNT_OF_NUM, MAX_AMOUNT_OF_NUM)
-    progression = get_progression(first_num, difference, amount_of_nums)
-    sliced_progression = progression[0:10]
-    index_of_miss_num = randint(MIN_INDEX_MISS_NUM, len(sliced_progression) - 1)
-    correct_answer = sliced_progression[index_of_miss_num]
+    progression = get_progression(first_num, difference, AMOUNT_OF_NUM)
+    index_of_miss_num = randint(MIN_INDEX_MISS_NUM, len(progression) - 1)
+    correct_answer = progression[index_of_miss_num]
     progression_with_hiden_num = get_progression_with_hiden_num(
-        sliced_progression, index_of_miss_num)
+        progression, index_of_miss_num)
     question = get_progression_converted_to_str(progression_with_hiden_num)
     return question, str(correct_answer)
