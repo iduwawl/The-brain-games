3b55ef1b8e6992b24446aff10a2238007c9d9c7c
Aleksandr Sukhomlinov <iduwawl@gmail.com>
2024-02-14 16:01:26 +0300

    refactor: Rename var

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,7 +3,7 @@ from random import randint
 GAME_RULE = "What number is missing in the progression?"
 MIN_FIRST_NUM = 1
 MAX_FIRST_NUM = 10
-AMOUNT_OF_NUM = 10
+AMOUNT_OF_NUMS = 10
 MIN_DIFF_PROGRESSION = 2
 MAX_DIFF_PROGRESSION = 9
 MIN_INDEX_MISS_NUM = 0
@@ -29,7 +29,7 @@ def get_progression_converted_to_str(progression) -> str:
 def get_question_and_correct_answer():
     first_num = randint(MIN_FIRST_NUM, MAX_FIRST_NUM)
     difference = randint(MIN_DIFF_PROGRESSION, MAX_DIFF_PROGRESSION)
-    progression = get_progression(first_num, difference, AMOUNT_OF_NUM)
+    progression = get_progression(first_num, difference, AMOUNT_OF_NUMS)
     index_of_miss_num = randint(MIN_INDEX_MISS_NUM, len(progression) - 1)
     correct_answer = progression[index_of_miss_num]
     progression_with_hiden_num = get_progression_with_hiden_num(
